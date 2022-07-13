5453bcf09676b63a65c4ad0d818a110c6ee63c3b
Steven Ke <spp72kimo@gmail.com>
2022-07-13 15:49:35 +0800

    fix the nav in add_article.php

--- add_article.php
+++ add_article.php
@@ -44,7 +44,7 @@
   </head>
   <body> 
     <nav>
-      <div class="navbar navbar-expand-md navbar-dark">
+      <div class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="container-fluid"><a class="navbar-brand" href="index.php">Steven's Blog</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarNav">
